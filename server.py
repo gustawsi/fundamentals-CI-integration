b2df6f0eff1a48d9d5baabda4b0f5e8fe84eec17
Adrelix <Adrelix@github.com>
2023-02-06 14:49:57 +0100

    Created a simple server with get/post request implemented. This will serve as a skeleton for the server.

--- server.py
+++ server.py
@@ -1,7 +1,48 @@
-require 'sinatra'
-require 'json'
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 
-post '/payload' do
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
-end
+hostName = "localhost"
+serverPort = 8011
+
+class Server(BaseHTTPRequestHandler):
+  
+  def do_GET(self):
+      self.send_response(200)
+      self.send_header("Content-type", "text/html")
+      self.end_headers()
+      self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+      self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+      self.wfile.write(bytes("<body>", "utf-8"))
+      self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+      self.wfile.write(bytes("</body></html>", "utf-8"))
+      #print out output info for given commit or list of commits if in top directory
+
+    
+  def do_POST(self):
+      self.send_response(200)
+      #pull from git
+      #compile
+          #if checking python code: Flake8 to lint (check if code has syntax errors)
+          #if other, check output of compile
+      #run test
+      #create directory and file with output info
+          #eg:
+              #date, branch, commit-id
+              #build: ok
+              #test: 7/7
+          #or:
+              #date, branch, commit-id
+              #build: no ok
+                  #error message?        #post to api.git (commit status) 
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), Server)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
