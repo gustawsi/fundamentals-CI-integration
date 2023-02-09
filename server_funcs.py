de64f585aa544247579391f2ea32517af3005095
adrelix <adrelix@github.com>
2023-02-09 10:34:28 +0100

    added flake8 checking on thebuild function, now returns 1 on success and 0 on failure

--- server_funcs.py
+++ server_funcs.py
@@ -5,16 +5,26 @@ import tempfile
 
 
 def build(body):
-	temp_dir = tempfile.TemporaryDirectory()
-	repo = git.Repo.clone_from(config.git_repo_url, os.path.join(config.temp_repo_path + temp_dir.name), branch="main")
 	#takes output from parse_post_json, clones the repo from git
 	#compiles code - lints python (flake8)
-	#return 0 on success, 1 on fail
+	#return 1 on success, 0 on fail
 
+	temp_dir = tempfile.TemporaryDirectory()
+	temp_path = config.temp_repo_path + temp_dir.name
+	git.Repo.clone_from(config.git_repo_url, os.path.join(temp_path), branch="git-compilation")
+	res = os.system("python3 " + temp_path + " flake8")
+	if res == 0:
+		return 1
+	else: 
+		return 0
+
+	
 def test():
+	print()
 	#runs tests
 
 def save_results():
+	print()
 	#creates folder for the commit results - perhaps a separate function called from here
 	#creates file with results in folder
 		#dependency versions and commit id included
@@ -29,4 +39,5 @@ def save_results():
                   #error message?        #post to api.git (commit status)
 
 def restore():
+	print()
 	#deletes the cloned repo and compiled code in preparation for next webhook
