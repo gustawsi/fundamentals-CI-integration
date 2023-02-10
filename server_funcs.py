c1ac650d5a3cfd8fe888d368151420a5a8b0891f
Levi <leyh@kth.se>
2023-02-10 10:25:17 +0100

    Revert ""test" added, checks passed assertions"
    This reverts commit 85d6c2f2c30208a7533d95a6170ac9e7c53bcd56.

--- server_funcs.py
+++ server_funcs.py
@@ -1,36 +1,10 @@
-import requests
-import unittest
-import subprocess
-
-
 def build(body):
 	#takes output from parse_post_json, clones the repo from git
 	#compiles code - lints python (flake8)
 	#return 0 on success, 1 on fail
 
-
-def test(temp_path):
-    """
-    function runs the test.py file from the server
-    and returns array with number of tests passed and total number of tests
-    """
-    test_path = temp_path + '/test/test.py'
-    
-    test_suite = unittest.TestLoader().loadTestsFromTestCase(test_path)
-    tests_run = unittest.TextTestRunner(verbosity=2).run(test_suite)
-    tests_passed = tests_run.testsRun - len(tests_run.failures)
-
-    return (tests_passed, tests_run.testsRun)
-
-
-    print('Result:', tests_passed.wasSuccessful())
-    for failure in tests_passed.failures:
-        print('Failure:', failure[0])
-
-    results = test.test_file_exists()
-    print("Passed:", results[0], "of", results[1], "tests.")
-
-  
+def test():
+	#runs tests
 
 def save_results():
 	#creates folder for the commit results - perhaps a separate function called from here
