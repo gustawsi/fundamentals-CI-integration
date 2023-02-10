cc1c6261a4823910146d292ba97e0051346b864d
anna-sara-maria <ak9522@gmail.com>
2023-02-10 17:35:26 +0100

    fix Notification
    added last piece of information needed to run send_email()

--- src/server_funcs.py
+++ src/server_funcs.py
@@ -149,7 +149,7 @@ def send_email(message):
     receiver_email = "gustawsi@ug.kth.se,adriankv@ug.kth.se"
     sender_email = "continuousintegration2023@gmail.com"
     # the password will be integrated into the code but not here on github :)
-    password = input(str("please enter your password : "))
+    #password = input(str("please enter your password : "))
     simple_email_context = ssl.create_default_context()
     smtp_port = 587			# Standard secure SMTP port
     smtp_server = "smtp.gmail.com"  # Google SMTP Server
@@ -157,7 +157,7 @@ def send_email(message):
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls(context=simple_email_context)
-        server.login(sender_email, password)
+        server.login(sender_email, "bmvk xgju anqh kloq")
         server.sendmail(sender_email, receiver_email, message)  # Sends email
         print("Email has been sent to", receiver_email)
 
