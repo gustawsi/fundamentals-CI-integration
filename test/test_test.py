441d08d0b5ad886f77973eab0852ca111c3a386d
gustawsi <58599084+gustawsi@users.noreply.github.com>
2023-02-23 21:52:41 +0100

    Update test_test.py

--- test/test_test.py
+++ test/test_test.py
@@ -9,7 +9,7 @@ class TestServer(unittest.TestCase):
         self.assertTrue(True)
     
     def test_file_exists(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
 
 if __name__ == "__main__" :
