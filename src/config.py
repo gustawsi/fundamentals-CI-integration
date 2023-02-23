3601b06b5b1698a5fa0317265786b5d9ff3f5a39
gustawsi <gsiedlarski@gmail.com>
2023-02-23 21:07:26 +0100

    Update config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 
-temp_repo_path = "/home/adam/Desktop/fundamentals-CI-integration"
-git_repo_url = "https://github.com/anna-sara-maria/fundamentals-CI-integration"
+temp_repo_path = "/home/gussie/kth/softfund/a2_CI_server/own-repo/temps"
+git_repo_url = "https://github.com/gustawsi/fundamentals-CI-integration"
