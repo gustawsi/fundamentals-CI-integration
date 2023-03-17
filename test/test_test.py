733254a11f77ff2ce410528d70e7226e1200b5be
gustawsi <58599084+gustawsi@users.noreply.github.com>
2023-03-17 09:27:39 +0100

    Update test_test.py

--- test/test_test.py
+++ test/test_test.py
@@ -4,8 +4,6 @@ sys.path.append('../')
 class TestServer(unittest.TestCase):
 
 
-    def test_dir_size_not_zero(self):
-        self.assertTrue(True)
     
     def test_file_exists(self):
         self.assertTrue(True)
